1a654b650228a6e6b2d2458aedd5cbe627f2b30a
Adam <adhocadam@protonmail.com>
2019-10-08 23:45:51 -0500

    ACS Translate form
    New form for the Settings UI that surfaces the MP properties for Azure Translate functionality

---
+++ ManagementPack/2016/SMLetsExchangeConnectorSettingsUI/Forms/AzureTranslate.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.EnterpriseManagement.UI.WpfWizardFramework;
+using Microsoft.EnterpriseManagement.UI.DataModel;
+using Microsoft.EnterpriseManagement.UI;
+
+namespace SMLetsExchangeConnectorSettingsUI
+{
+    /// <summary>
+    /// Interaction logic for AzureTranslate.xaml
+    /// </summary>
+
+    public partial class AzureTranslate : WizardRegularPageBase
+    {
+        AdminSettingWizardData settings;
+
+        private AzureTranslate adminSettingWizardData = null;
+
+        public AzureTranslate(WizardData wizardData)
+        {
+            InitializeComponent();
+            this.DataContext = wizardData;
+            this.adminSettingWizardData = this.DataContext as AzureTranslate;
+        }
+
+        private void WizardRegularPageBase_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
