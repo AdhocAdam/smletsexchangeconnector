3a92c46290e2265f00e2f850341503611181dc38
Adam <adhocadam@protonmail.com>
2019-12-26 11:34:49 -0600

    syntax error
    previous commit broke switch logic via }

--- smletsExchangeConnector.ps1
+++ smletsExchangeConnector.ps1
@@ -1719,7 +1719,6 @@ function Update-WorkItem ($message, $wiType, $workItemID) 
                             {
                                 "\[$completedKeyword]" {Set-SCSMObject -SMObject $workItem -PropertyHashtable @{"Status" = "ActivityStatusEnum.Completed$"; "ActualEndDate" = (get-date).ToUniversalTime(); "Notes" = "$($workItem.Notes)$($activityImplementer.Name) @ $(get-date): $commentToAdd `n"} @scsmMGMTParams; if ($ceScripts) { Invoke-AfterCompleted }}
                                 "\[$skipKeyword]" {Set-SCSMObject -SMObject $workItem -PropertyHashtable @{"Status" = "ActivityStatusEnum.Skipped$"; "Skip" = $true; "ActualEndDate" = (get-date).ToUniversalTime(); "Notes" = "$($workItem.Notes)$($activityImplementer.Name) @ $(get-date): $commentToAdd `n"} @scsmMGMTParams; if ($ceScripts) { Invoke-AfterSkipped }}
-                            }
                                 default {
                                     $parentWorkItem = Get-SCSMWorkItemParent $workItem.Get_Id().Guid
                                     switch ($parentWorkItem.Classname)
