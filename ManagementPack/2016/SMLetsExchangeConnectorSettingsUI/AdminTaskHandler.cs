605b27ace05bf95db63ebb5ba118fd5e4e0fb4a8
Adam <adhocadam@protonmail.com>
2020-07-25 17:50:39 -0500

    Azure Speech integration (v2.x) (#199)
    * Azure Speech integration
    
    Adds support to the powershell to work with Azure speech services to transcribe audio to text.
    
    * azure speech readme
    
    Updating readme to include description of Azure Speech integration
    
    * Azure Speech properties
    
    Extending the Settings class with support for Azure Speech functionality
    
    * Azure Speech code behind
    
    Adding references for Settings MP code behind to reference the Settings MP and UI
    
    * speech forms
    
    Adding UI forms for Azure Speech integration
    
    * referencing new ui forms
    
    Updating the main form loader to reference the new Azure Speech Services forms
    
    * speech icon for new ui
    
    Azure Speech icon for new configuration page

--- ManagementPack/2016/SMLetsExchangeConnectorSettingsUI/AdminTaskHandler.cs
+++ ManagementPack/2016/SMLetsExchangeConnectorSettingsUI/AdminTaskHandler.cs
@@ -73,6 +73,7 @@ namespace SMLetsExchangeConnectorSettingsUI
             wizard.AddLast(new WizardStep("A.I.", typeof(ArtificialIntelligence), wizard.WizardData));
             wizard.AddLast(new WizardStep("Translation", typeof(AzureTranslate), wizard.WizardData));
             wizard.AddLast(new WizardStep("Vision", typeof(AzureVision), wizard.WizardData));
+            wizard.AddLast(new WizardStep("Transcription", typeof(AzureSpeech), wizard.WizardData));
             wizard.AddLast(new WizardStep("About", typeof(AboutForm), wizard.WizardData));
 
             //Show the property page 
---
+++ ManagementPack/2016/SMLetsExchangeConnectorSettingsUI/Forms/AzureSpeech.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.EnterpriseManagement.UI.WpfWizardFramework;
+using Microsoft.EnterpriseManagement.UI.DataModel;
+using Microsoft.EnterpriseManagement.UI;
+
+namespace SMLetsExchangeConnectorSettingsUI
+{
+    /// <summary>
+    /// Interaction logic for AzureSpeech.xaml
+    /// </summary>
+
+    public partial class AzureSpeech : WizardRegularPageBase
+    {
+        AdminSettingWizardData settings;
+
+        private AzureSpeech adminSettingWizardData = null;
+
+        public AzureSpeech(WizardData wizardData)
+        {
+            InitializeComponent();
+            this.DataContext = wizardData;
+            this.adminSettingWizardData = this.DataContext as AzureSpeech;
+        }
+
+        private void WizardRegularPageBase_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
