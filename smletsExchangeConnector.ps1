5a52856575f211514988378c03be071e23ccdd9f
Adam <adhocadam@protonmail.com>
2023-03-14 12:38:28 -0500

    v5.0.2.2 (#446)
    updating inline release notes

--- smletsExchangeConnector.ps1
+++ smletsExchangeConnector.ps1
@@ -20,6 +20,7 @@ Requires: PowerShell 4+, SMlets, and Exchange Web Services API (already installe
     Signed/Encrypted option: .NET 4.5 is required to use MimeKit.dll
 Misc: The Release Record functionality does not exist in this as no out of box (or 3rd party) Type Projection exists to serve this purpose.
     You would have to create your own Type Projection in order to leverage this.
+Version: 5.0.2 = #444 - Bug, Fix for Templates that contain one or many Activities
 Version: 5.0.1 = #441 - Enhancement, Logging for when a user attempts to vote on a Review Activity that they are not a Reviewer on
                 #440 - Enhancement, standardizing remaining functions param blocks and clarify logging event
                 #439 - Enhancement, ItemClass logging event
