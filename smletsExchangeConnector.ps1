3127720079c5b2c3d2723bd2be2afd2c9ff1ef40
Adam <6636040+AdhocAdam@users.noreply.github.com>
2025-05-23 13:00:49 -0500

    notes + comment to add
    initial commit, meets parity with stock connector to prevent the comment + current notes from exceeding 4,000 characters. introduces a Custom Event to override this behavior.

--- smletsExchangeConnector.ps1
+++ smletsExchangeConnector.ps1
@@ -2299,7 +2299,16 @@ function Update-WorkItem
                                         }
                                     }
                                     default {
-                                        Set-SCSMObject -SMObject $workItem -PropertyHashtable @{"Notes" = "$($workItem.Notes)$($commentLeftBy.Name) @ $(get-date): $commentToAdd `n"} @scsmMGMTParams
+                                        $commentToAdd = $workItem.Notes + "$($workItem.Notes)$($commentLeftBy.Name) @ $(get-date): $commentToAdd `n"
+                                        if ($commentToAdd.Length -gt 4000) {
+                                            if ($loggingLevel -ge 3) {
+                                                New-SMEXCOEvent -Source "Update-WorkItem" -EventId 14 -Severity "Error" -LogMessage "Activity notes entry is too long. Max total length is 4000 characters. This text has not been added to the activity notes and no further text will be added automatically by the Exchange connector. If you would like to alter this behavior, modify the Invoke-AfterMANoteFailure Custom Event."
+                                            }
+                                            if ($ceScripts) { Invoke-AfterMANoteFailure }
+                                        }
+                                        else {
+                                            Set-SCSMObject -SMObject $workItem -PropertyHashtable @{"Notes" = $commentToAdd} @scsmMGMTParams
+                                        }
                                     }
                                 }
                             }
