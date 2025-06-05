3bfc47f829a450966b3327e395b9087691c336bf
Adam <6636040+AdhocAdam@users.noreply.github.com>
2025-06-05 12:40:36 -0500

    event verbiage
    updating the event message to be more accurate of what is happening

--- smletsExchangeConnector.ps1
+++ smletsExchangeConnector.ps1
@@ -2301,7 +2301,7 @@ function Update-WorkItem
                                     default {
                                         if (("$($workItem.Notes)$($commentLeftBy.Name) @ $(get-date): $commentToAdd `n").Length -gt 4000) {
                                             if ($loggingLevel -ge 3) {
-                                                New-SMEXCOEvent -Source "Update-WorkItem" -EventId 14 -Severity "Error" -LogMessage "Activity notes entry is too long. Max total length is 4000 characters. This text has not been added to the activity notes and no further text will be added automatically by the SMLets Exchange Connector. If you would like to alter this behavior, modify the Invoke-AfterMANoteFailure Custom Event."
+                                                New-SMEXCOEvent -Source "Update-WorkItem" -EventId 14 -Severity "Error" -LogMessage "Activity notes entry is too long. Max total length is 4000 characters. This text has not been added to the activity notes. If you would like to alter this behavior, modify the Invoke-AfterMANoteFailure Custom Event."
                                             }
                                             if ($ceScripts) { Invoke-AfterMANoteFailure }
                                         }
