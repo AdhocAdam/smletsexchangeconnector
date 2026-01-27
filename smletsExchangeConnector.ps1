0a2f24b6254b252b187cabf74b8b552b98c19212
Adam <6636040+AdhocAdam@users.noreply.github.com>
2026-01-26 18:52:42 -0600

    updating notes
    adding version notes

--- smletsExchangeConnector.ps1
+++ smletsExchangeConnector.ps1
@@ -20,6 +20,7 @@ Requires: PowerShell 4+, SMlets, and Exchange Web Services API (already installe
     Signed/Encrypted option: .NET 4.5 is required to use MimeKit.dll
 Misc: The Release Record functionality does not exist in this as no out of box (or 3rd party) Type Projection exists to serve this purpose.
     You would have to create your own Type Projection in order to leverage this.
+Version: 6.0.0 = #476 - Enhancement, Support for Microsoft Graph/Retirement of EWS for Exchange Online 
 Version: 5.0.5 = #494 - Bug, Update-WorkItem on MAs does not take MA Notes into account
                  #497 - Bug, Event log exposes password when not using Run As Accounts
                  #480 - Bug, Missing Cloud Activity Prefix
