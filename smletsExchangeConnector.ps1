7538ca71bc6f9bf8fa97a789e3d2ce5c526679de
Adam <6636040+AdhocAdam@users.noreply.github.com>
2026-06-11 14:13:31 -0500

    handle inline attachments (#529)
    * handle inline attachments
    
    the HasAttachments property for Graph messages does not honor inline attachments. Removing part of this check to ensure attachments regardless of where they are on a message are always attempted to be retrieved when using Exchange Online
    
    * update verbiage
    
    since HasAttachments is no longer/can no longer be relied upon, remove it from the comment explaining the purpose of the call

--- smletsExchangeConnector.ps1
+++ smletsExchangeConnector.ps1
@@ -5282,8 +5282,8 @@ foreach ($message in $inbox)
             ItemClass           = $message.ItemClass
         }
 
-        #if we have the HasAttachments property and we're using Exchange Online/Graph. Call graph for attachments and set them in the existing property
-        if ($message.HasAttachments -and $UseExchangeOnline) {
+        #Call graph for attachments and set them in the existing property
+        if ($UseExchangeOnline) {
             $attachmentEndpoint = Invoke-RestMethod -uri "https://$azureSubdomain.microsoft.$azureTLD/v1.0/me/messages/$($email.ID)/attachments" -Headers @{Authorization = "Bearer $($tokenReqResponse.access_token)" }
             $email.Attachments = $attachmentEndpoint.value
         }
@@ -5356,8 +5356,8 @@ foreach ($message in $inbox)
             ItemClass         = $message.ItemClass
         }
 
-        #if we have the HasAttachments property and we're using Exchange Online/Graph. Call graph for attachments and set them in the existing property
-        if ($message.HasAttachments -and $UseExchangeOnline) {
+        #Call graph for attachments and set them in the existing property
+        if ($UseExchangeOnline) {
             $attachmentEndpoint = Invoke-RestMethod -uri "https://$azureSubdomain.microsoft.$azureTLD/v1.0/me/messages/$($email.ID)/attachments" -Headers @{Authorization = "Bearer $($tokenReqResponse.access_token)" }
             $email.Attachments = $attachmentEndpoint.value
         }
